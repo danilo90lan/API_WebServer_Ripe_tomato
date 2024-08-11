d71ec4ccd44899adc1b7f8b454f70159499292b8
danilo90lan <15630@coderacademy.edu.au>
2024-08-11 15:24:33 +1000

    added delete routes for actors and movies

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from sqlalchemy.exc import IntegrityError
 from datetime import timedelta
 
@@ -162,7 +162,8 @@ def add_users():
         User(
             name="User 1",
             email="ciao@gmail.com",
-            password=bcrypt.generate_password_hash("123456").decode('utf8')
+            password=bcrypt.generate_password_hash("123456").decode('utf8'),
+            admin=True
         ),
 
         User(
@@ -259,7 +260,8 @@ def register_user():
         user = User(
             name=body_data["name"],
             email=body_data["email"],
-            password=hashed_password
+            password=hashed_password,
+            admin=body_data["admin"]
         )
         # Add it to the session
         db.session.add(user)
@@ -298,7 +300,56 @@ def login_user():
     # else return an error message
     else:
         return {"error":"Invalid email or password"},401
+    
+# DELETE records from movies
+@app.route("/movies/<int:id>", methods=["DELETE"])
+@jwt_required()
+def delete_movie(id):
+    if not authoriseAsAdmn():
+        return {"authorization error":"you are not authorized"},403
+    else:
+        statement = db.select(Movie).filter_by(id_movie=id)
+        result = db.session.scalar(statement)
 
+        if result:
+            db.session.delete(result)
+            db.session.commit()
+            return {"delete":f"movie with id: {id} deleted"}
+        else:
+            return {"error":f"the movie with id: {id} doesn not exist!"}
+        
+# DELETE records from actors
+@app.route("/actors/<int:id>", methods=["DELETE"])
+@jwt_required()
+def delete_actor(id):
+    if not authoriseAsAdmn():
+        return {"authorization error":"you are not authorized"},403
+    else:
+        statement = db.select(Actor).filter_by(id_actor=id)
+        result = db.session.scalar(statement)
+
+        if result:
+            db.session.delete(result)
+            db.session.commit()
+            return {"delete":f"actor with id: {id} deleted"}
+        else:
+            return {"error":f"the actor with id: {id} doesn not exist!"}
+
+
+def authoriseAsAdmn():
+    # we receive the token and we look which user ID the token is linked to
+    # get the id of the user from the jwt token using get_jwt_identity() function
+    user_id = get_jwt_identity()
+    #find the user in the db with the id
+    statement = db.select(User).filter_by(id=user_id)
+    result = db.session.scalar(statement)
+    # check if the user is admin or not 
+    admin = result.admin
+    # return True if the admin is True of False if the admin is False
+    if admin:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     app.run(debug=True)
