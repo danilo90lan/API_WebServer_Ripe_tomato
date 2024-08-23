139221e23f28b308c89fc766e08c54a98fe5ca3e
danilo90lan <15630@coderacademy.edu.au>
2024-08-24 00:53:53 +1000

    connected actors and movies models

--- models/actor.py
+++ models/actor.py
@@ -1,4 +1,5 @@
 from init import db, ma
+from marshmallow import fields
 
 class Actor(db.Model):
     __tablename__ = "actors"
@@ -11,9 +12,13 @@ class Actor(db.Model):
 
 class ActorSchema(ma.Schema):
     class Meta:
+        #  we include "movies" in the schema that referes to the back_populate value from the other relationship
+        #  table and since this is a the one side relationship we cam receive many movies from the other table
+        # so we create a list of movie
+        movie = fields.List(fields.Nested("MovieSchema", exclude=["actor"]))
         fields = ("id_actor", "actor_first_name",
-                  "actor_last_name", "country", "dob")
-        
+                  "actor_last_name", "country", "dob", "movies")
+
 
 actor_schema = ActorSchema()
 actors_schema = ActorSchema(many=True)
--- models/movie.py
+++ models/movie.py
@@ -1,4 +1,5 @@
 from init import db, ma
+from marshmallow import fields
 
 class Movie(db.Model):
     __tablename__ = "movies"
@@ -8,9 +9,23 @@ class Movie(db.Model):
     length = db.Column(db.String(100))
     release_date = db.Column(db.Date)
 
+    # foreign key (table_name && id name variable)
+    # if we use the relationship below created, we dont need to use this field
+    actor_id = db.Column(db.Integer, db.ForeignKey("actors.id_actor"), nullable = False)
+
+    # if we want to fetch more information from the Foreign key other than ID only, 
+    # we define a relationship between the two models
+    # First parameter is the MODEL NAME of the other table, and second parameter is the Movie fields (TABLE NAME)
+    # that we want to get from the other "side" (Actor table). It's a two ways retrieving
+    actor = db.relationship("Actor", back_populates="movies")
+
 class MovieSchema(ma.Schema):
+    # we need to "unpack" the value of the user in order to be deserialized from marshmallow.
+    # it's not a column vale so it cannot be recognize from marshmallow but since we already have the schema 
+    # to unpack the value, we used it ("UserSchema") from the User model
+    actor = fields.Nested("ActorSchema", only=["id", "name", "email"])
     class Meta:
-        fields = ("id_movie", "title", "genre", "length", "release_date")
+        fields = ("id_movie", "title", "genre", "length", "release_date", "actor_id")
 
 movie_schema = MovieSchema()
 movies_schema = MovieSchema(many=True)
